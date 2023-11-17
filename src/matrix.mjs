62ffde75f94c1839be25ff08a7b10e69fcd9adcb
Miguel Rust <prettygoodstudiosapps@gmail.com>
2023-11-16 20:56:03 -0700

    Making it fail again.

--- src/matrix.mjs
+++ src/matrix.mjs
@@ -211,9 +211,9 @@ export class Matrix {
             throw Error('Invalid matrices provided for dot product');
         }
         let dotProduct = 0;
-        for (let i = 0; i < myDimensions; i++) {
-            dotProduct += getMyValue(i) * getVectorValue(i);
-        }
+        // for (let i = 0; i < myDimensions; i++) {
+        //     dotProduct += getMyValue(i) * getVectorValue(i);
+        // }
         // hello
         return dotProduct;
     }
